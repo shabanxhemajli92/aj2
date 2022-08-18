344ce1d0a74c1a7ae8290807b50251f583978a6f
Shaban <shabanxhemajli@live.com>
2022-08-18 15:04:19 +0200

    hh

--- aj2.py
+++ aj2.py
@@ -1,6 +1,27 @@
 import os
 import time
 import datetime
+import smtplib
+from email.mime.multipart import*
+from email.mime.text      import*
 from colorama import *
 from art  import *
+from email.message    import EmailMessage
 
+# initialzing colorama
+init()
+
+
+#email function  
+def gmail_send(subject, message, from_mail, to, password):
+    global link
+    server = smtplib.SMTP('smtp.gmail.com', 587)
+    server.starttls()
+    server.login(from_mail, password)
+    msg            = EmailMessage()
+    message        = f'{message}'
+    msg.set_content(message)
+    msg['Subject'] = subject
+    msg['From']    = from_mail
+    msg['To']      = to
+    server.send_message(msg)
